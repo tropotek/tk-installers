4f5ba159d9ad77c51a4507264d4e05ad9f2e2118
mifsudm <mifsudm@unimelb.edu.au>
2019-06-05 11:06:49 +1000

    ~Auto: Commit

--- Tk/Composer/SetupEvent.php
+++ Tk/Composer/SetupEvent.php
@@ -209,7 +209,6 @@ STR;
                 if ($config->get('sql.migrate.list')) {
                     $sqlMigrateList = $config->get('sql.migrate.list');
                 }
-                print_r($sqlMigrateList);
                 foreach ($sqlMigrateList as $searchPath) {
                     if (!is_dir($searchPath)) continue;
                     $dirItr = new \RecursiveDirectoryIterator($searchPath, \RecursiveIteratorIterator::CHILD_FIRST);
